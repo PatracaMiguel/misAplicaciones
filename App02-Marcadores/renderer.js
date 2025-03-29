c8bb9c2bf17946f4778820cbe720a8fb61ddc36f
Miguel <miguelangelpatracalagunes@gmail.com>
2025-03-28 21:22:57 -0600

    App02-Marcadores

---
+++ App02-Marcadores/renderer.js
@@ -0,0 +1,11 @@
+class Marcador{
+    constructor(){
+        this.mensajeError = document.querySelector('.mensaje-error');
+        this.marcadorUrl = document.querySelector('.creacion-marcador-url');
+        this.marcadorBoton = document.querySelector('.creacion-marcador-boton');
+        this.formularioMarcadores = document.querySelector('.creacion-marcador-formulario');
+        this.marcadores = document.querySelector('.marcadores');
+        this.eliminarMarcadores = document.querySelector('.remover-marcadores');
+        
+    }
+}
